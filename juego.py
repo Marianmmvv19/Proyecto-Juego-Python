2419d2fe4b7cee30ba9dbc91347fa031509951ac
Marian Vasquez <marianvasquez@gmail..com>
2024-09-18 16:05:02 -0400

    Terminé de modificar la pantalla.

---
+++ juego.py
@@ -0,0 +1,12 @@
+import pygame
+import ramdom 
+import math 
+import sys
+import os
+
+#Inicializar pygame
+pygame.init()
+
+#Establece el tamaño de ña pantalla
+screen_width = 800
+screen_height = 500
